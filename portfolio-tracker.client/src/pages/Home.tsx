3b8439d7b5387a137c7a5c76de09343e3631227a
Shakirudeen Lasisi <s.lasisi@waam3d.com>
2025-11-08 12:12:29 +0000

    scroll to feature from nav

--- portfolio-tracker.client/src/pages/Home.tsx
+++ portfolio-tracker.client/src/pages/Home.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import Hero from "../components/Hero";
 import Features from "../components/Features";
 import "../index.css";
 
 const Home: React.FC = () => {
+    const { hash } = useLocation();
+    useEffect(() => {
+        if (hash) {
+            const element = document.querySelector(hash);
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    }, [hash]);
     return (
         <main>
             <Hero />

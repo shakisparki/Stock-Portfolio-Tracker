f91f6958fd745044ce9b9d3487dba70010c971a7
Shakirudeen Lasisi <s.lasisi@waam3d.com>
2025-12-13 14:53:06 +0000

    define prices and dummy controller

---
+++ Portfolio-Tracker.Server/Controllers/PricesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio_Tracker.Server.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Portfolio_Tracker.Server.Controllers
+{
+    [Route("api/market/[controller]")]
+    [ApiController]
+    public class PricesController : ControllerBase
+    {
+        // GET api/<PricesController>/5
+        [HttpGet]
+        public PricesResponse Get([FromQuery] string tickers)
+        {
+            var tickersList = tickers.Split(',');
+            return new PricesResponse
+            {
+                Prices = [.. tickersList.Select(x =>
+                    new MarketPrice
+                    {
+                        Ticker = x.Trim(),
+                        CurrentPrice = 150.25m,
+                        ChangePercent = 1.5m,
+                        Historical =
+                        [
+                            new() { Date = DateTime.Now.AddDays(-1), Value = 148.00m },
+                            new() { Date = DateTime.Now.AddDays(-2), Value = 149.00m },
+                            new() { Date = DateTime.Now.AddDays(-3), Value = 147.50m }
+                        ]
+                    }
+                    )]
+            };
+        }
+    }
+}
---
+++ Portfolio-Tracker.Server/Models/Prices.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Portfolio_Tracker.Server.Models
+{
+    public class PricesResponse
+    {
+        public List<MarketPrice> Prices { get; set; } = [];
+    }
+
+    public class MarketPrice
+    {
+        public required string Ticker { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public decimal ChangePercent { get; set; }
+        public List<HistoricalPrice> Historical { get; set; } = [];
+    }
+
+    public class HistoricalPrice
+    {
+        public DateTime Date { get; set; }
+        public decimal Value { get; set; }
+    }
+}

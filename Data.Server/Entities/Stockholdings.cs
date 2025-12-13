c83b3290cb0b4131b7bad307297bce87fe13fb30
Shakirudeen Lasisi <s.lasisi@waam3d.com>
2025-12-13 10:53:36 +0000

    define stockholding and add migraiton

--- Data.Server/Entities/Stockholdings.cs
+++ Data.Server/Entities/Stockholdings.cs
@@ -2,5 +2,10 @@
 {
     public class Stockholdings
     {
+        public int Id { get; set; }
+        public required string Ticker { get; set; }
+        public decimal Shares { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public DateTime PurchaseDate { get; set; }
     }
 }
---
+++ Data.Server/Migrations/20251213104925_AddStockHolding.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Data.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStockHolding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stockholdings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Ticker = table.Column<string>(type: "text", nullable: false),
+                    Shares = table.Column<decimal>(type: "numeric", nullable: false),
+                    PurchasePrice = table.Column<decimal>(type: "numeric", nullable: false),
+                    PurchaseDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stockholdings", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stockholdings");
+        }
+    }
+}
